14b745e6628de0957dc5cff072f5d92b90198d60
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2025-10-30 16:09:01 +0800

    Initial Commit

---
+++ AUV Control Panel/AUV_Control_PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUV_Control_PanelApp.swift
+//  AUV Control Panel
+//
+//  Created by KamHo on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AUV_Control_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
